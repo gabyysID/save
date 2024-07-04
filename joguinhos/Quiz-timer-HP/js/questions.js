d9b393058c6be0ba3d4f0a80ab4be7d3f0e3462f
gabyysID <gabriellyleitesocial@gmail.com>
2024-07-04 13:14:57 -0300

    Add files via upload

---
+++ joguinhos/Quiz-timer-HP/js/questions.js
@@ -0,0 +1,147 @@
+//criando um mini mini banco como dados para as perguntas.
+let questions = [
+    {
+    numb: 1,
+    question: "Quem se sacrificou para retirar o Medalhão de Sonserina original da caverna?",
+    answer: "Regulus Black",
+    options: [
+      "Regulus Black",
+      "Monstro",
+      "Albus Dumbledore",
+      "Sirius Black"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Qual o nome da poção usada para mudar de forma??",
+    answer: "Poção Polissuco",
+    options: [
+      "Felix Felicis",
+      "Poção do Morto-Vivo",
+      "Amortentia",
+      "Poção Polissuco"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Quem foi o diretor de Hogwarts após a morte de Dumbledore?",
+    answer: "Severeus Snape",
+    options: [
+      "Lord Voldemort",
+      "Minerva McGonagall",
+      "Severeus Snape",
+      "Rúbeo Hagrid"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Qual era o fantasma da Grifinória?",
+    answer: "Nick Quase-sem-cabeça",
+    options: [
+      "Dama Cinzeta",
+      "Nick Quase-sem-cabeça",
+      "Frei Gorducho",
+      "Barão Sangrento",
+    ]
+  },
+    {
+    numb: 5,
+    question: "Qual era a posição de Harry no quadribol?",
+    answer: "Apanhador",
+    options: [
+      "Apanhador",
+      "Goleiro",
+      "Artilheiro",
+      "Batedor"
+      
+    ]
+  },
+    {
+    numb: 6,
+    question: "Qual o nome da poção usada para sentir o cheiro da pessoa que ama?",
+    answer: "Amortentia",
+    options: [
+      "Felix Felicis",
+      "Poção do Morto-Vivo",
+      "Amortentia",
+      "Poção Polissuco"
+    ]
+  },
+  {
+    numb: 7,
+    question: "Quando Bicuço volta para Hogwarts, ele vem com outro nome? Qual é?",
+    answer: "Asafugaz",
+    options: [
+      "Asafugaz",
+      "Bico de Aço",
+      "Ele vem com outro nome",
+      "Ele não troca de nome"
+    ]
+  },
+  {
+    numb: 8,
+    question: "Em uma dessas escolhas tem somente animagos. Qual?",
+    answer: "James Potter, Minerva McGonagall e Sirius",
+    options: [
+      "Remo Lupin, James Potter, Rabicho e Minerva McGonagall",
+      "James Potter, Minerva McGonagall e Sirius",
+      "Remo Lupin, James Potter, Rabicho e Sirius",
+      "Nenhuma das alternativas."
+    ]
+  },
+  {
+    numb: 9,
+    question: "Prisioneiro de Azkaban: no Egito, o que Fred e George tentaram fazer?",
+    answer: "Tentaram trancar o Percy na pirâmide",
+    options: [
+      "Tentaram trancar Percy em um sarcófago",
+      "Nenhuma das alternativas",
+      "Tentaram jogar Ron no rio",
+      "Tentaram trancar o Percy na pirâmide"
+    ]
+  },
+  {
+    numb: 10,
+    question: "Qual era a desculpa de Madame Maxime para ser tão alta?",
+    answer: "'Tenho ossos graúdos'",
+    options: [
+      "'Tenho ossos graúdos'",
+      "'Fui enfeitiçada'",
+      "'Tomei uma poção quando era pequena'",
+      "'Eu sou meio gigante'"
+    ]
+  },
+  {
+    numb: 11,
+    question: "O que Pirraça fazia toda vez que Umbridge falava?",
+    answer: "Fazia sons de pum com a boca",
+    options: [
+      "Gemia",
+      "Começava a cantar bem alto e estridentemente",
+      "Fazia sons de pum com a boca",
+      "Falava insultos"
+    ]
+  },
+  {
+    numb: 12,
+    question: "No segundo ano, que celebração é administrada por Gilderoy Lockheart?",
+    answer: "Dia dos Namorados",
+    options: [
+      "Dia dos Namorados",
+      "Natal",
+      "Hallowen",
+      "Ação de Graças"
+    ]
+  },
+
+  //   numb: ?,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
