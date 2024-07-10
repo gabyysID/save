55fc018a9dfc144a816439eef00fe961c10fdd10
gabyysID <gabriellyleitesocial@gmail.com>
2024-07-10 14:16:40 -0300

    Create app.js

---
+++ react/Hook/app.js
@@ -0,0 +1,12 @@
+import AsyncAwait from './AsyncAwait';
+import HookState from './HookState';
+
+function App(){
+  return (
+    <div>
+      <h1>teste</h1>
+      <HookState/>
+    <AsyncAwait/>
+    </div>
+  );
+}
